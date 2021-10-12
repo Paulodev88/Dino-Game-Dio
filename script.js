3aafef5466f443371f636f2474f29d1f3b68db02
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-10-12 20:47:53 -0300

    create: function cactus

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 
 let isJump = false;
 const dino =  document.querySelector('.dino');
+const background = document.querySelector('.background')
 
 document.addEventListener('keyup', handleKeyUp)
 
@@ -43,3 +44,26 @@ function jump() {
         
    }, 20) 
 }
+
+function createCactus() {
+    const cactus = document.createElement('div')
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus')
+    cactus.style.left = 1000 + 'px'
+    background.appendChild(cactus)
+
+    let leftInterval = setInterval(() => {        
+
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval)
+            background.removeChild(cactus)
+        }else{
+            cactusPosition -=10;
+            cactus.style.left = cactusPosition + 'px'
+        }
+    },20)
+}
+
+createCactus()
+
