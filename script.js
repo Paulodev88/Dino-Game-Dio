d177dd15c3457397fc640c8bb9eccd27a6d0cd9c
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-10-12 20:32:43 -0300

    create: function jump

--- script.js
+++ script.js
@@ -3,3 +3,43 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+let isJump = false;
+const dino =  document.querySelector('.dino');
+
+document.addEventListener('keyup', handleKeyUp)
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+       if (!isJump) {
+            jump();
+       }
+    }
+}
+
+function jump() {
+   let position = 0;
+   isJump = true
+   let upInterval = setInterval(() => {
+
+    if (position >= 150) {
+        clearInterval(upInterval);
+
+      
+        let downInterval = setInterval(() => {
+            if(position <= 0){
+                clearInterval(downInterval)
+                isJump = false
+            }
+            position -=10;
+            dino.style.bottom = position + 'px';
+    },20)
+
+    } else{
+
+        position +=10; 
+
+        dino.style.bottom = position + 'px';
+    }
+        
+   }, 20) 
+}
